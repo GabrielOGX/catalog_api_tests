82614888b89d501eec5888c4f55cc5506ab2b119
gabriel_m_silva10 <gabriel_m_silva10@estudante.sesisenai.org.br>
2024-10-25 21:42:14 -0300

    add unit tests

---
+++ src/test/java/com/catalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,49 @@
+package com.catalog.repositories;
+
+import com.catalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+
+        long existingId = 1L;
+
+        repository.deleteById(existingId);
+
+        Optional<Product> result = repository.findById(existingId);
+
+        Assertions.assertFalse(result.isPresent());
+    }
+
+
+    @Test
+    public void testSaveProduct(){
+    Product product = new Product();
+    product.setName("Test Product");
+    product.setDescription("testando");
+    product.setPrice(5000.00);
+    product.setImgUrl("localhost/img");
+   // product.setDate(Instant.parse("2024-10-25T21:21:00.00+03:00"));
+
+    Product savedProduct = repository.save(product);
+
+    assertThat(savedProduct).isNotNull();
+    assertThat(savedProduct.getName()).isEqualTo("Test Product");
+
+
+    }
+}
